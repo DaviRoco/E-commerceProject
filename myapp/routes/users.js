c38b251361a85e377f83946e4a9d2e6403194e6c
David Rodríguez Coto <david@creamanalytics.com>
2023-04-01 10:28:26 -0600

    01-Connect the app and database

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,9 +1,7 @@
-var express = require('express');
+var express = require("express");
+const db = require("./queries");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get("/", db.getUsers);
 
 module.exports = router;
